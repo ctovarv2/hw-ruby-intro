c9db61e73582487e31d25682db452e18cb3fc025
ctovarv2 <ctovarv2@gmail.com>
2018-01-25 08:54:42 -0600

    All parts finished and tested.

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -6,31 +6,33 @@ def sum arr
   # YOUR CODE HERE
   if arr.empty?
 	return 0
-  else 
-	arr.sum
+  else
+	return arr.sum
   end
 end
 
 def max_2_sum arr
   # YOUR CODE HERE
-  if arr.empty? 
+  if arr.empty?
 	return 0
   elsif arr.length == 1
 	return arr[0]
   else
     sum = 0
-	maxElements = arr.max(2)
-	maxElements.each{|x| sum += x}
+	arr.max(2).to_a.each{|x| sum += x}
 	return sum
   end
 end
 
 def sum_to_n? arr, n
   # YOUR CODE HERE
+  
+  #Cheacks if the array is empty or of length 1
   if arr.empty? || arr.length == 1
 	return false
   else
-    
+	#Returns true if a permutation of 2 digits add up to n
+    return arr.permutation(2).any?{|x,y| x + y == n}
   end
 end
 
@@ -38,18 +40,63 @@ end
 
 def hello(name)
   # YOUR CODE HERE
+  return "Hello, " + name
 end
 
 def starts_with_consonant? s
   # YOUR CODE HERE
+  
+  #Checks if string is empty
+  if s.empty?
+	   return false
+  else
+		#Cheacks if first character is a letter and if its not a vowel
+	   if !(s =~ /\A[aeiouAEIOU]/) && !(s =~ /\A[^a-z, ^A-Z]/)
+		     return true
+	   else
+		     return false
+	   end
+  end
 end
 
 def binary_multiple_of_4? s
   # YOUR CODE HERE
+  
+  #Checks if string is empty or contains a letter
+  if s.empty? or !(s !~ /[a-zA-Z, 2-9]/)
+    return false
+  else
+	#Checks if divisible by 4 or if its a single bit(0)
+	if ((s =~ /(00)$/) != nil) or ((s =~ /^0${1}/) != nil)
+		return true
+	end
+  end
 end
 
 # Part 3
 
 class BookInStock
 # YOUR CODE HERE
+
+	#Creates Getters and Setters
+	attr_accessor :isbn, :price
+	
+	
+	#Constructor
+	def initialize(isbn, price)
+		if isbn.empty?
+			raise ArgumentError.new("ISBN argument cannot be empty.")
+		end
+		if price <= 0.0
+			raise ArgumentError.new("Invalid Price argument.")
+		end
+		@isbn = isbn
+		@price = price
+	end
+	
+	
+	#Displays Price in correct format
+	def price_as_string
+		sprintf("$%#.2f", @price)
+	end
 end
